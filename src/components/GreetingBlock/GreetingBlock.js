e231e799e92fda0c5b8df36c70ecdb1d6c795321
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-11-20 14:58:26 +0300

    greeting block added

--- src/components/GreetingBlock/GreetingBlock.js
+++ src/components/GreetingBlock/GreetingBlock.js
@@ -1,7 +1,41 @@
 import React from "react";
-import "./GreetingBlock";
+import { useEffect } from "react";
+import $ from "jquery";
+import "./GreetingBlock.scss";
 function GreetingBlock() {
-  return <div>GreetingBlock</div>;
+  useEffect(() => {
+    (function () {
+      let words = ["Lorem", "Ipsum", "dolor", "sit", "amet"];
+
+      let i = -1;
+      setInterval(function () {
+        // $("#words").removeClass("test-animation-on-apear");
+
+        $("#words").fadeOut(function () {
+          $(this)
+            .html(words[(i = (i + 1) % words.length)])
+            .fadeIn();
+          //.addClass("test-animation-on-apear");
+        });
+      }, 2000);
+    })();
+  });
+
+  /**
+   *
+   *
+   *
+   *
+   */
+  return (
+    <>
+      <div class="container">
+        <div class="animated-heading">
+          <h1 id="words"></h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default GreetingBlock;
